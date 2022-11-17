15ffdb48f216368be4aa6e81648692ff3db3f757
unknown <timafra2@mallon.com.br>
2022-11-17 17:31:27 -0300

    adicionando arquivo seed no databaseSeeder

--- database/seeds/TodoSeeder.php
+++ database/seeds/TodoSeeder.php
@@ -5,19 +5,15 @@ use Illuminate\Database\Seeder;
 class TodoSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Para cada usuário cria 10 seeds de to-dos, e para cada to-dos ele cria 10 to-dos-tasks
      *
      * @return void
      */
     public function run()
     {
         App\User::all()->each(function($user){//para cada funcionario cria 10 todos //each é um loop
-            $user->todos()->saveMany(
-                factory(Todo::class, 10)->make() //usa-se o make porque quem salva no banco é o saveMany()
-            )->each(function($todo){
-                $todo->tasks()->saveMany(
-                    factory(TodoTask::class)->make()
-                );
+            $user->todos()->saveMany(factory(Todo::class, 10)->make())->each(function($todo){
+                $todo->tasks()->saveMany(factory(TodoTask::class, 10)->make());//usa-se o make porque quem salva no banco é o saveMany()
             });
         });
     }
