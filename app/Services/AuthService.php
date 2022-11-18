23d301f3dc095ca0050d4e028efa0a8b0c810957
unknown <timafra2@mallon.com.br>
2022-11-18 14:48:46 -0300

    configurando exception do login

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,9 +2,26 @@
 
 namespace App\Services;
 
+use App\Exceptions\LoginInvalidException;
+
 class AuthService{
 
-    public function login_user(){
+    public function login_user(string $email, string $password){
+
+        $login =[
+            'email' => $email,
+            'password' => $password
+        ];
+
+        if(!$token = auth()->attempt($login)){ //responsável por logar o usuário 
+            
+            throw new LoginInvalidException();
+        }
+
+        return [
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ];
 
         dd('Auth Service acessado com sucesso');
     }
